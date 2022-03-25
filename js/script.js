c8204f7b963e9a62e0eb88a5debbc373cf39d0b5
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-25 16:50:40 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit2-05-HTML
+// March 25 2022
 
-// 'use strict'
+'use strict'
 /**
- * Alerts "Hello, World!"
+ * Calculates money earned by the user and the government using the user's input.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  const hours = parseFloat(document.getElementById("hours").value)
+  const hourlypay = parseFloat(document.getElementById("hourlypay").value)
+  document.getElementById("calculation").innerHTML = "<br><p>You'll earn $" + ((hours * hourlypay) * (1 - 0.18)).toFixed(2) + "</p><br><p>The government earns $" + ((hours * hourlypay) * 0.18).toFixed(2) + "</p>"
 }
